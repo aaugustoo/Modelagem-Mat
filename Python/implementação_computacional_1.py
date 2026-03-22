a35a0100853149edff16b40f2f17cd271f67f8da
aaugustoo <belarminoleiteaugustosergio@gmail.com>
2026-03-22 07:03:18 -0300

    Reorganização da IC1 em Python + criação do arquivo da IC1 em Lua

--- Python/implementação_computacional_1.py
+++ Python/implementação_computacional_1.py
@@ -18,21 +18,11 @@ h = 0.1
 # Valores do eixo x estarão contidos na lista X
 X = []
 
-
-
-# Derivada y' = f(x,y) = -y³
-def f(x, y):
-    resultado = -(y ** 3)
-    return resultado
-
-
-
 # Preenchimento de X
 valor = x0
 while valor <= limite:
     X.append(valor)
     valor += h
-
 
 
 # Cada Y representa uma lista com os resultados de cada método de resolução indicado
@@ -43,30 +33,33 @@ Y_Euler = [0] * len(X)
 Y_Runge_Kutta = [0] * len(X)
 
 
+# Derivada y' = f(x,y) = -y³
+def f(x, y):
+    resultado = -(y ** 3)
+    return resultado
+
 
 # Resolução analítica, Y_Analitico começa recebendo y0 na primeira posição graças ao PVI
-n = 1
 Y_Analitico[0] = y0
+n = 1
 while n <= len(X) - 1:
     Y_Analitico[n] = 1/(math.sqrt(2 * X[n] + 1))
     n += 1
 
 
-
 # Resolução pelo método de Euler, Y_Euler começa recebendo y0 na primeira posição por definição
 # y(n) = y(n-1) + h * f(Xn-1, Yn-1)
-n = 1
 Y_Euler[0] = y0
+n = 1
 while n <= len(X) - 1:
     Y_Euler[n] = Y_Euler[n - 1] + h * f(X[n - 1], Y_Euler[n - 1])
     n += 1
 
 
-
 # Resolução pelo método de Runge-Kutta, Y_Runge_Kutta começa recebendo y0 na primeira posição por definição
 # y(n) = y(n-1) + 1/6 * h * (Kn1 + 2Kn2 + 2Kn3 + Kn4)
-n = 1
 Y_Runge_Kutta[0] = y0
+n = 1
 while n <= len(X) - 1:
     Kn1 = f(X[n - 1], Y_Runge_Kutta[n - 1])
     Kn2 = f(X[n - 1] + 1/2 * h, Y_Runge_Kutta[n - 1] + 1/2 * h * Kn1)
@@ -75,7 +68,6 @@ while n <= len(X) - 1:
 
     Y_Runge_Kutta[n] = Y_Runge_Kutta[n - 1] + h * 1/6 * (Kn1 + 2*Kn2 + 2*Kn3 + Kn4)
     n += 1
-
 
 
 # Plotagem do gráfico com os resultados dos três métodos
